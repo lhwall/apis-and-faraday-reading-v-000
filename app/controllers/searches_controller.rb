06484253ccbb84c55de16de5e0a4c8470e82a892
Lucy Wall <lhwall@gmail.com>
2019-02-06 14:35:21 +0000

    Automatically backed up by Learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,7 +3,7 @@ class SearchesController < ApplicationController
   end
 
   def foursquare
-    Faraday.get
+    @resp = Faraday.get
     "https://api.foursquare.com/v2/venues/search" do |req|
       req.params['client_id'] = WPFHOMJ0433AIKNJ5XWLNE4FJOUJK23X2I5NSKKIL15DHR54
       req.params['client_secret'] = 1VGXYGNO0LVGYQCMOZB5BODIWJJ1NGVKLVJ32DK4GRKBMMWE
