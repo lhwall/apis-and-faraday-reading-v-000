b8a5bad966730b5228cdf5153ef04d53958ff3f2
Lucy Wall <lhwall@gmail.com>
2019-02-06 14:48:02 +0000

    Automatically backed up by Learn

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -18,6 +18,11 @@ if @resp.success?
 else
   @error = body["meta"]["errorDetail"]
 end
+
+rescue Faraday::ConnectionFailed
+  @error = "There was a timeout. Please try again."
+end
+
 render 'search'
 
    end
